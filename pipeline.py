860d4f88b370cfd48eb78d001883585f2adeb838
hammad <hammadshamim642@gmail.com>
2024-02-17 17:41:16 +0500

    create dataproc cluster
    update pipeline.py to create cluster

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,13 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.google.cloud.hooks.dataproc import DataprocHook#
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocCreateClusterOperator,
+    DataprocSubmitPySparkJobOperator,
+    DataprocDeleteClusterOperator,
+    ClusterGenerator
+)
+
 from datetime import (
     datetime, 
     timedelta
@@ -12,11 +20,20 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-with DAG("load-sales-dm-file-data-to-bq-daily-v1.0",default_args=default_args,schedule_interval="30 10 * * *",max_active_runs=1,catchup=False) as dag:
+with DAG("load-sales-dm-file-data-to-bq-daily-v1.0",default_args=default_args,schedule_interval=None,max_active_runs=1,catchup=False) as dag:
         
 
     dataproc_cluster_running = DummyOperator(
         task_id='dataproc_cluster_running'
     )
+    create_cluster = DataprocCreateClusterOperator(
+        task_id='create_dataproc_cluster',
+        cluster_name='dataproc-cluster',  # Dynamically named cluster
+        num_workers=2,
+        master_machine_type='n2-standard-2',
+        worker_machine_type='n2-standard-2',
+        region='us-central1',  # Replace with your desired region
+        project_id='playground-s-11-e3c70ff1',
+    )
 
-    dataproc_cluster_running
+    dataproc_cluster_running >> create_cluster
