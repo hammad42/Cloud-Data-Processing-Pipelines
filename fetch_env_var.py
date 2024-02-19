c6152ca36474b1197a80861113e90c9e829c6467
hammad <hammadshamim642@gmail.com>
2024-02-19 17:14:34 +0500

    update fetch.py

--- fetch_env_var.py
+++ fetch_env_var.py
@@ -1,6 +1,22 @@
-import os
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import (
+    datetime, 
+    timedelta
+)
 
-print("Fetching environment variables:")
+def fetch_and_print_vars():
+    import os
+    for key, value in os.environ.items():
+        print(f"{key}: {value}")
 
-for key, value in os.environ.items():
-    print(f"{key}: {value}")
+with DAG(
+    dag_id="fetch_env_vars_os",
+    start_date=datetime.now(),
+    schedule_interval=None,
+) as dag:
+
+    fetch_vars = PythonOperator(
+        task_id="fetch_vars",
+        python_callable=fetch_and_print_vars,
+    )
