50d98a347b28432e448b08fade31d126bdb90dc2
hammad <hammadshamim642@gmail.com>
2024-03-22 23:39:56 +0500

    update insertion_script_database_to_database.py

--- src/extras/inserting_script_database_to_database.py
+++ src/extras/inserting_script_database_to_database.py
@@ -1,45 +1,51 @@
-from sqlalchemy import create_engine
-import sqlalchemy.exc
-import pandas as pd 
-import os
+def insertion(table):  
+    from sqlalchemy import create_engine
+    import sqlalchemy.exc
+    import pandas as pd 
+    import os
 
-database_username = os.environ.get("database_username")
-database_password = os.environ.get("database_password")
-database_name = os.environ.get("database_name")
-database_host = os.environ.get("database_host") # Find this in your instance details
+    database_username = os.environ.get("database_username")
+    database_password = os.environ.get("database_password")
+    database_name = os.environ.get("database_name")
+    database_host = os.environ.get("database_host") # Find this in your instance details
 
-database_username_azure = os.environ.get("database_username_azure")
-database_password_azure = os.environ.get("database_password_azure")
-database_name_azure = os.environ.get("database_name_azure")
-database_host_azure = os.environ.get("database_host_azure") # Find this in your instance details transactionaldataserver.database.windows.net
-table="dept_manager"
+    database_username_azure = os.environ.get("database_username_azure")
+    database_password_azure = os.environ.get("database_password_azure")
+    database_name_azure = os.environ.get("database_name_azure")
+    database_host_azure = os.environ.get("database_host_azure") # Find this in your instance details transactionaldataserver.database.windows.net
+    # table="titles"
 
-# **Database 1 (MySQL - Source)**
-db1_connection_string = 'mysql+pymysql://{}:{}@{}/{}'.format(
-    database_username, database_password, database_host, database_name
-)
-db1_engine = create_engine(db1_connection_string)
+    # **Database 1 (MySQL - Source)**
+    db1_connection_string = 'mysql+pymysql://{}:{}@{}/{}'.format(
+        database_username, database_password, database_host, database_name
+    )
+    db1_engine = create_engine(db1_connection_string)
 
-# **Database 2 (Azure SQL Server - Destination)**
-db2_engine_url = f'mssql+pyodbc://{database_username_azure}:{database_password_azure}@{database_host_azure}/{database_name_azure}?driver=ODBC+Driver+17+for+SQL+Server'
-db2_engine = create_engine(db2_engine_url)
+    # **Database 2 (Azure SQL Server - Destination)**
+    db2_engine_url = f'mssql+pyodbc://{database_username_azure}:{database_password_azure}@{database_host_azure}/{database_name_azure}?driver=ODBC+Driver+17+for+SQL+Server'
+    db2_engine = create_engine(db2_engine_url)
 
-# **1. Query Data from Database 1**
-query = """
-SELECT *
-FROM {}
-where emp_no >= 10200 and emp_no < 10300
-""".format(table)
-print(query)
-try:
-    df = pd.read_sql(query, db1_engine)
-    print("Data retrieved from the first database.")
-except sqlalchemy.exc.OperationalError as e:
-    print(f"Query from the first database failed: {e}")
+    # **1. Query Data from Database 1**
+    query = """
+    SELECT *
+    FROM {}
+    where emp_no >= 10700 and emp_no < 10800
+    """.format(table)
+    print(query)
+    try:
+        df = pd.read_sql(query, db1_engine)
+        print("Data retrieved from the first database.")
+    except sqlalchemy.exc.OperationalError as e:
+        print(f"Query from the first database failed: {e}")
 
-# **2. Load into the Azure Database**
-try:
-    df.to_sql(table, db2_engine, if_exists='append', index=False)  
-    print("Data loaded into the Azure database.")
-except sqlalchemy.exc.OperationalError as e:
-    print(f"Loading into the Azure database failed: {e}")
+    # **2. Load into the Azure Database**
+    try:
+        result=df.to_sql(table, db2_engine, if_exists='append', index=False)  
+        inserted_rows = result
+        print(f"Data loaded into the Azure database. Inserted rows: {inserted_rows}")
+    except sqlalchemy.exc.OperationalError as e:
+        print(f"Loading into the Azure database failed: {e}")
+
+tables=("employees","dept_emp","dept_manager","salaries","titles")
+for table in tables:
+    insertion(table)
